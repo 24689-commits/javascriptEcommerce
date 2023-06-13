c72f802a06b2ba9c700b7e1115024399c433b322
Aphelele Joyi <aphelelejoyi@gmail.com>
2023-06-13 04:17:04 +0200

    crud

--- script/admin.js
+++ script/admin.js
@@ -1,129 +1,171 @@
 // Function to open the modal
 function openModal() {
-    document.getElementById("modal").style.display = "block";
+  document.getElementById("modal").style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+// Function to generate a new ID for an item
+function generateItemId() {
+  // Retrieve the last stored ID from local storage
+  var lastItemId = localStorage.getItem("lastItemId");
+
+  // Generate a new ID based on the last stored ID
+  var newItemId = lastItemId ? parseInt(lastItemId) + 1 : 1;
+
+  // Update the lastItemId in local storage
+  localStorage.setItem("lastItemId", newItemId);
+
+  return newItemId;
+}
+
+// Function to add an item to the table and Local Storage
+function addItem() {
+  // Get the input values from the modal
+  var itemImage = document.getElementById("itemImage").value;
+  var itemDescription = document.getElementById("itemDescription").value;
+  var itemPrice = document.getElementById("itemPrice").value;
+
+  // Generate a new ID for the item
+  var itemId = generateItemId();
+
+  // Create a new table row
+  var table = document.getElementById("tblAdmin");
+  var newRow = table.insertRow(table.rows.length);
+
+  // Insert cells into the new row
+  var cellId = newRow.insertCell(0);
+  var cellImage = newRow.insertCell(1);
+  var cellDescription = newRow.insertCell(2);
+  var cellPrice = newRow.insertCell(3);
+  var cellAction = newRow.insertCell(4);
+
+  // Set the cell values
+  cellId.innerHTML = itemId;
+  cellImage.innerHTML = "<img src='" + itemImage + "' alt='Item Image' width='100'>";
+  cellDescription.innerHTML = itemDescription;
+  cellPrice.innerHTML = itemPrice;
+  cellAction.innerHTML = "<button onclick='editItem(this)'>Edit</button> <button onclick='deleteItem(this)'>Delete</button>";
+
+  // Save the item data to Local Storage
+  saveItemToLocalStorage(itemId, itemImage, itemDescription, itemPrice);
+
+  // Clear the input fields
+  document.getElementById("itemImage").value = "";
+  document.getElementById("itemDescription").value = "";
+  document.getElementById("itemPrice").value = "";
+
+  // Close the modal
+  closeModal();
+}
+
+// Function to edit an item in the table and Local Storage
+function editItem(button) {
+  var row = button.parentNode.parentNode;
+  var cells = row.getElementsByTagName("td");
+
+  // Get the cell values
+  var itemId = cells[0].innerHTML;
+  var itemImage = cells[1].innerHTML;
+  var itemDescription = cells[2].innerHTML;
+  var itemPrice = cells[3].innerHTML;
+
+  // Set the input values in the modal
+  document.getElementById("itemId").value = itemId;
+  document.getElementById("itemImage").value = itemImage;
+  document.getElementById("itemDescription").value = itemDescription;
+  document.getElementById("itemPrice").value = itemPrice;
+
+  // Remove the row from the table
+  row.parentNode.removeChild(row);
+
+  // Open the modal for editing
+  openModal();
+}
+
+// Function to delete an item from the table and Local Storage
+function deleteItem(button) {
+  var row = button.parentNode.parentNode;
+  var itemId = row.cells[0].innerHTML;
+
+  // Remove the row from the table
+  row.parentNode.removeChild(row);
+
+  // Remove the item data from Local Storage
+  removeItemFromLocalStorage(itemId);
+}
+
+// Function to save an item to Local Storage
+function saveItemToLocalStorage(itemId, itemImage, itemDescription, itemPrice) {
+  // Retrieve existing item data from Local Storage
+  var items = localStorage.getItem("items");
+  var itemList = items ? JSON.parse(items) : [];
+
+  // Create a new item object
+  var newItem = {
+    itemId: itemId,
+    itemImage: itemImage,
+    itemDescription: itemDescription,
+    itemPrice: itemPrice
+  };
+
+  // Add the new item to the item list
+  itemList.push(newItem);
+
+  // Update the items data in Local Storage
+  localStorage.setItem("items", JSON.stringify(itemList));
+}
+
+// Function to remove an item from Local Storage
+function removeItemFromLocalStorage(itemId) {
+  // Retrieve existing item data from Local Storage
+  var items = localStorage.getItem("items");
+  var itemList = items ? JSON.parse(items) : [];
+
+  // Find the index of the item to remove
+  var index = itemList.findIndex(function(item) {
+    return item.itemId === itemId;
+  });
+
+  // Remove the item from the item list
+  if (index !== -1) {
+    itemList.splice(index, 1);
   }
-  
-  // Function to close the modal
-  function closeModal() {
-    document.getElementById("modal").style.display = "none";
-  }
-  
-  // Function to add an item to the table
-  function addItem() {
-    // Get the input values from the modal
-    let itemId = document.getElementById("itemId").value;
-    let itemImage = document.getElementById("itemImage").value;
-    let itemDescription = document.getElementById("itemDescription").value;
-    let itemPrice = document.getElementById("itemPrice").value;
-  
-    // Create a new table row
-    let table = document.getElementById("tblAdmin");
-    let newRow = table.insertRow(table.rows.length);
-  
+
+  // Update the items data in Local Storage
+  localStorage.setItem("items", JSON.stringify(itemList));
+}
+
+// Function to initialize the table with items from Local Storage
+function initializeTable() {
+  // Retrieve existing item data from Local Storage
+  var items = localStorage.getItem("items");
+  var itemList = items ? JSON.parse(items) : [];
+
+  var table = document.getElementById("tblAdmin");
+
+  // Iterate over the item list and populate the table
+  itemList.forEach(function(item) {
+    var newRow = table.insertRow(table.rows.length);
+
     // Insert cells into the new row
-    let cellId = newRow.insertCell(0);
-    let cellImage = newRow.insertCell(1);
-    let cellDescription = newRow.insertCell(2);
-    let cellPrice = newRow.insertCell(3);
-    let cellAction = newRow.insertCell(4);
-  
+    var cellId = newRow.insertCell(0);
+    var cellImage = newRow.insertCell(1);
+    var cellDescription = newRow.insertCell(2);
+    var cellPrice = newRow.insertCell(3);
+    var cellAction = newRow.insertCell(4);
+
     // Set the cell values
-    cellId.innerHTML = itemId;
-    cellImage.innerHTML = "<img src='" + itemImage + "' alt='Item Image' width='100'>";
-    cellDescription.innerHTML = itemDescription;
-    cellPrice.innerHTML = itemPrice;
+    cellId.innerHTML = item.itemId;
+    cellImage.innerHTML = "<img src='" + item.itemImage + "' alt='Item Image' width='100'>";
+    cellDescription.innerHTML = item.itemDescription;
+    cellPrice.innerHTML = item.itemPrice;
     cellAction.innerHTML = "<button onclick='editItem(this)'>Edit</button> <button onclick='deleteItem(this)'>Delete</button>";
-  
-    // Clear the input fields
-    document.getElementById("itemId").value = "";
-    document.getElementById("itemImage").value = "";
-    document.getElementById("itemDescription").value = "";
-    document.getElementById("itemPrice").value = "";
-  
-    // Close the modal
-    closeModal();
-  
-    // Save the item to local storage
-    saveItemToLocalStorage(itemId, itemImage, itemDescription, itemPrice);
-  }
-  
-  // Function to save an item to local storage
-  function saveItemToLocalStorage(itemId, itemImage, itemDescription, itemPrice) {
-    // Get existing items from local storage
-    let items = JSON.parse(localStorage.getItem("items")) || [];
-  
-    // Create a new item object
-    let newItem = {
-      itemId: itemId,
-      itemImage: itemImage,
-      itemDescription: itemDescription,
-      itemPrice: itemPrice
-    };
-  
-    // Add the new item to the array
-    items.push(newItem);
-  
-    // Store the updated items in local storage
-    localStorage.setItem("items", JSON.stringify(items));
-  }
-  
-  // Function to edit an item in the table
-  function editItem(button) {
-    let row = button.parentNode.parentNode;
-    let cells = row.getElementsByTagName("td");
-  
-    // Get the cell values
-    let itemId = cells[0].innerHTML;
-    let itemImage = cells[1].innerHTML;
-    let itemDescription = cells[2].innerHTML;
-    let itemPrice = cells[3].innerHTML;
-  
-    // Set the input values in the modal
-    document.getElementById("itemId").value = itemId;
-    document.getElementById("itemImage").value = itemImage;
-    document.getElementById("itemDescription").value = itemDescription;
-    document.getElementById("itemPrice").value = itemPrice;
-  
-    // Remove the row from the table
-    row.parentNode.removeChild(row);
-  
-    // Open the modal for editing
-    openModal();
-  }
-  
-  // Function to delete an item from the table
-  function deleteItem(button) {
-    let row = button.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-  }
-  
-  // Load items from local storage on page load
-  function loadItemsFromLocalStorage() {
-    let items = JSON.parse(localStorage.getItem("items")) || [];
-  
-    for (let i = 0; i < items.length; i++) {
-      let item = items[i];
-  
-      // Create a new table row
-      let table = document.getElementById("tblAdmin");
-      let newRow = table.insertRow(table.rows.length);
-  
-      // Insert cells into the new row
-      let cellId = newRow.insertCell(0);
-      let cellImage = newRow.insertCell(1);
-      let cellDescription = newRow.insertCell(2);
-      let cellPrice = newRow.insertCell(3);
-      let cellAction = newRow.insertCell(4);
-  
-      // Set the cell values
-      cellId.innerHTML = item.itemId;
-      cellImage.innerHTML = "<img src='" + item.itemImage + "' alt='Item Image' width='100'>";
-      cellDescription.innerHTML = item.itemDescription;
-      cellPrice.innerHTML = item.itemPrice;
-      cellAction.innerHTML = "<button onclick='editItem(this)'>Edit</button> <button onclick='deleteItem(this)'>Delete</button>";
-    }
-  }
-  
-  // Call the function to load items from local storage on page load
-  loadItemsFromLocalStorage();
-  
+  });
+}
+
+// Initialize the table when the page loads
+initializeTable();
--- script/products.js
+++ script/products.js
@@ -1,72 +1,48 @@
-// let data = [
-//     {
-//       id: 1,
-//       image: 'https://i.postimg.cc/pLFtvHzg/Coffee-1.jpg',
-//       description: 'sdfghjkcghjk'
-//     },
-//     {
-//       id: 2,
-//       image: 'https://i.postimg.cc/pLFtvHzg/Coffee-1.jpg',
-//       description: 'sdfghjkcghjk'
-//     },
-//     {
-//       id: 3,
-//       image: 'https://i.postimg.cc/pLFtvHzg/Coffee-1.jpg',
-//       description: 'sdfghjkcghjk'
-//     }
-//   ];
-  
-  
-  
-//   data.forEach(item => {
-//     html += `
-//       <div>
-//         <h2>ID: ${item.id}</h2>
-//         <img src="${item.image}" alt="Image ${item.id}" class="img-fluid">
-//         <p>${item.description}</p>
-//       </div>
-//     `;
-//   });
-  
-
-
-
-  
-
-        //   let data = [
-        //       {
-        //         id: 1,
-        //         image: 'https://i.postimg.cc/pLFtvHzg/Coffee-1.jpg',
-        //         description: 'sdfghjkcghjk'
-        //       },
-        //       {
-        //         id: 2,
-        //         image: 'https://i.postimg.cc/pLFtvHzg/Coffee-1.jpg',
-        //         description: 'sdfghjkcghjk'
-        //       },
-        //       {
-        //         id: 3,
-        //         image: 'https://i.postimg.cc/pLFtvHzg/Coffee-1.jpg',
-        //         description: 'sdfghjkcghjk'
-        //       }
-        //   ];
-  
-        //   let html = '';
-          
-        //   data.forEach(item => {
-        //       html += `
-        //           <div>
-        //               <h2>ID: ${item.id}</h2>
-        //               <img src="${item.image}" alt="Image ${item.id}" class="img-fluid">
-        //               <p>${item.description}</p>
-        //           </div>
-        //       `;
-        //   });
-  
-        //   let prodElement = document.getElementById("prod");
-        //   prodElement.innerHTML = html;
-
-
-     
-  
-       
+document.addEventListener("DOMContentLoaded", function() {
+        let prod = document.getElementById("prod");
+        let products = [
+            {
+                id: 1,
+                image: 'https://i.postimg.cc/t4C5HkQ6/1kg-Brazil-Medium-Dark-Roast-Beans.jpg',
+                description: 'Product 1 description',
+                price: 9.99
+            },
+            {
+                id: 2,
+                image: 'https://i.postimg.cc/t4C5HkQ6/1kg-Brazil-Medium-Dark-Roast-Beans.jpg',
+                description: 'Product 2 description',
+                price: 109.99
+            },
+            // ... add more products here ...
+        ];
+    
+        products.forEach((product) => {
+            prod.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <img src="${product.image}" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                    <h5 class="card-title">Product ${product.id}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <p class="card-text">Price: $${product.price}</p>
+                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#checkoutModal">Add to Cart</button>
+                </div>
+            </div>`;
+        });
+    
+        // Get the modal element
+        let checkoutModal = document.getElementById("checkoutModal");
+    
+        // Handle the Add to Cart button click event
+        let addToCartButtons = document.querySelectorAll(".btn.btn-primary[data-bs-toggle='modal']");
+        addToCartButtons.forEach((button) => {
+            button.addEventListener("click", function() {
+                // Perform the checkout logic here
+                // You can show a success message, update the cart, etc.
+    
+                // Close the modal after checkout
+                let modalInstance = bootstrap.Modal.getInstance(checkoutModal);
+                modalInstance.hide();
+            });
+        });
+    });
+    
